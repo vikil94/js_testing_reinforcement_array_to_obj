2addd5494de5f270c367b714ff7059274c9bc1a2
Vikil Naik <vikilnaik37@gmail.com>
2019-04-09 11:38:20 -0400

    finished excercise

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 function arrayToObj() {
-   // your code goes here  
+    // your code goes here  
+    return array.reduce((accumulator, i) => {
+        accumulator[i[0]] = i[1];
+        return accumulator;
+
+
+    }, {});
+
+
+
+
 }
 
 module.exports = arrayToObj;
